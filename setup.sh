6b59c2326d281d7d37ab00f4c84ab97d57b31140
Ilja Leiko <leikoilja@gmail.com>
2023-05-09 15:10:30 +0200

    Add lazygit to aliases + config file

--- setup.sh
+++ setup.sh
@@ -7,19 +7,17 @@
 DIR="/Users/leikoilja/Development/Dev_settings/dotfiles/symulinks"
 
 # Setup symbolic links
-ln -hfs "$DIR/.alacritty.yml" ~/.alacritty.yml
 ln -hfs "$DIR/.agignore" ~/.agignore
+ln -hfs "$DIR/.alacritty.yml" ~/.alacritty.yml
 ln -hfs "$DIR/.aliases" ~/.aliases
 ln -hfs "$DIR/.autoenv" ~/.autoenv
 ln -hfs "$DIR/.bashrc" ~/.bashrc
-ln -hfs "$DIR/.coc-settings.json" ~/.config/nvim/coc-settings.json
-ln -hfs "$DIR/init.lua" ~/.config/nvim/init.lua
-ln -hfs "$DIR/lua" ~/.config/nvim/lua
 ln -hfs "$DIR/.coc-settings.json" ~/.config/nvim/coc-settings.json
 ln -hfs "$DIR/.editorconfig" ~/.editorconfig
 ln -hfs "$DIR/.eslintrc.js" ~/.eslintrc.js
 ln -hfs "$DIR/.gitconfig" ~/.gitconfig
 ln -hfs "$DIR/.gitignore" ~/.gitignore
+ln -hfs "$DIR/.lazygit_config.yml" ~/.lazygit_config.yml
 ln -hfs "$DIR/.ssh" ~/.ssh
 ln -hfs "$DIR/.tmux.conf" ~/.tmux.conf
 ln -hfs "$DIR/.tmux.conf.local" ~/.tmux.conf.local
@@ -28,3 +26,5 @@ ln -hfs "$DIR/.tmuxinator.zsh" ~/.tmuxinator.zsh
 ln -hfs "$DIR/.vifmrc" ~/.config/vifm/vifmrc
 ln -hfs "$DIR/.vimrc" ~/.vimrc
 ln -hfs "$DIR/.zshrc_mac" ~/.zshrc
+ln -hfs "$DIR/init.lua" ~/.config/nvim/init.lua
+ln -hfs "$DIR/lua" ~/.config/nvim/lua
