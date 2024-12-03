4c430c18a0cfe3a9d2e8226f7c695ac65d2ffe34
Ilja Leiko <leikoilja@gmail.com>
2024-12-03 09:08:22 +0100

    fix(codecompanion): inline assistant keymapping

--- symlinks/nvim/lua/plugins/whichkey.lua
+++ symlinks/nvim/lua/plugins/whichkey.lua
@@ -7,7 +7,7 @@ return {
       { "<C-a>", "<cmd>CodeCompanionActions<cr>", mode = { "n", "v" }, noremap = true, silent = true },
       { "<leader>at", "<cmd>CodeCompanionChat Toggle<cr>", mode = { "n", "v" }, noremap = true, silent = true },
       { "<leader>ag", "<cmd>CodeCompanionChat Add<cr>", mode = "v", noremap = true, silent = true },
-      { "<leader>ac", "<cmd>CodeCompanion Inline Assistant<cr>", mode = "v", noremap = true, silent = true },
+      { "<leader>ac", "<cmd>CodeCompanion<cr>", mode = "v", noremap = true, silent = true },
     },
   },
 }
