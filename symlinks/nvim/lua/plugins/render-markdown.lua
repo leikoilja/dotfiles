93e2802285c200dc39dced5833b8f8f0e4662fba
Ilja Leiko <leikoilja@gmail.com>
2025-02-05 11:00:54 +0100

    refactor(nvim): markdown rendering

--- symlinks/nvim/lua/plugins/render-markdown.lua
+++ symlinks/nvim/lua/plugins/render-markdown.lua
@@ -2,17 +2,25 @@ return {
   "MeanderingProgrammer/render-markdown.nvim",
   config = function()
     require("render-markdown").setup({
+      enabled = true,
       bullet = {
         left_pad = 1,
         right_pad = 1,
       },
       heading = {
-        width = "block",
-        boarder = true,
-        border_virtual = true,
-        left_margin = 0.5,
-        left_pad = 0.5,
-        right_pad = 0.5,
+        enabled = true,
+        icons = {},
+        backgrounds = {
+          "GruvboxYellow",
+          "GruvboxOrange",
+          "GruvboxPurple",
+          "GruvboxPurple",
+          "GruvboxPurple",
+          "GruvboxPurple",
+        },
+      },
+      win_options = {
+        conceallevel = { default = 0, rendered = 0 },
       },
     })
   end,
