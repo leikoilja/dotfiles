2146e7ebfc8097f18c200342c052fa24c3c314c7
Ilja Leiko <leikoilja@gmail.com>
2026-03-07 06:47:59 +0100

    fix(nvim): bump codesnap

--- symlinks/nvim/lua/plugins/codesnap.lua
+++ symlinks/nvim/lua/plugins/codesnap.lua
@@ -1,3 +1,24 @@
--- return {
---   { "mistricky/codesnap.nvim", build = "make", opts = { watermark = "" } },
--- }
+return {
+  {
+    "mistricky/codesnap.nvim",
+    tag = "v2.0.0",
+    opts = {
+      snapshot_config = {
+        watermark = {
+          content = "CodeSnap.nvim",
+          font_family = "Pacifico",
+          color = "#ffffff",
+        },
+        code_config = {
+          breadcrumbs = {
+            enable = true,
+            separator = "/",
+            color = "#80848b",
+            font_family = "CaskaydiaCove Nerd Font",
+          },
+        },
+        show_line_number = true,
+      },
+    },
+  },
+}
