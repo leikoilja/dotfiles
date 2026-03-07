27cbbc8e707fb1e5d9be5a9a88ba9588ec7ac370
Ilja Leiko <leikoilja@gmail.com>
2026-03-07 08:13:23 +0100

    fix(nvim): lazyload codesnap

--- symlinks/nvim/lua/plugins/codesnap.lua
+++ symlinks/nvim/lua/plugins/codesnap.lua
@@ -1,24 +1,23 @@
 return {
-  {
-    "mistricky/codesnap.nvim",
-    tag = "v2.0.0",
-    opts = {
-      snapshot_config = {
-        watermark = {
-          content = "CodeSnap.nvim",
-          font_family = "Pacifico",
-          color = "#ffffff",
-        },
-        code_config = {
-          breadcrumbs = {
-            enable = true,
-            separator = "/",
-            color = "#80848b",
-            font_family = "CaskaydiaCove Nerd Font",
-          },
-        },
-        show_line_number = true,
+  "mistricky/codesnap.nvim",
+  tag = "v2.0.0",
+  cmd = { "CodeSnap", "CodeSnapSave", "CodeSnapASCII" },
+  opts = {
+    snapshot_config = {
+      watermark = {
+        content = "CodeSnap.nvim",
+        font_family = "Pacifico",
+        color = "#ffffff",
       },
+      code_config = {
+        breadcrumbs = {
+          enable = true,
+          separator = "/",
+          color = "#80848b",
+          font_family = "CaskaydiaCove Nerd Font",
+        },
+      },
+      show_line_number = true,
     },
   },
 }
